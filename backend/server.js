89349241c8c19f086cc9bc224e2529171b96f713
Goon Gupta <goongupta515@gmail.com>
2026-07-12 17:49:48 +0530

    Backend setup completed and MongoDB connected

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log("========== FULL ERROR ==========");
+    console.dir(err, { depth: null });
+    console.log("================================");
+  });
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Backend is running...");
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
